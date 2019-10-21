f7ed6eb2370c834133a4872649930abb63f96ce8
Maksim <rises128@gmail.com>
2019-10-21 21:32:25 +0500

    подправил основное

--- lesson_3/main.js
+++ lesson_3/main.js
@@ -37,9 +37,9 @@ console.log( money % 30 );
 
 if ( budgetDay > 800 ) {
   console.log('Высокий уровень дохода');
-} else if ( budgetDay > 300 && budgetDay <= 800 ) {
+} else if (  800 <= budgetDay > 300 ) {
   console.log( 'Средний уровень дохода' );
-} else if ( budgetDay > 0 && budgetDay <= 300 ) {
+} else if ( 300 <= budgetDay > 0 ) {
   console.log( 'Низкий уровень дохода' );
 } else {
   console.log( 'Что-то пошло не так' );
