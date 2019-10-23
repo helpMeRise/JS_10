a1c072893cf1907a6cabe1afd45b47fbd15802eb
Maksim <rises128@gmail.com>
2019-10-23 23:30:58 +0500

    подправил основное

--- lesson_5/main.js
+++ lesson_5/main.js
@@ -42,15 +42,20 @@ let getExpensesMonth = function() {
   for ( let i = 0; i < 2; i++ ){
     if ( i === 0 ) {
       question1 = prompt('Введите обязательную статью расходов', 'Квартплата');
+      do {
+        check = +prompt('Во сколько это обойдется', 10000);
+      }
+      while ( isNaN(check) || check === '' || check === null );
     }
-    if ( i === 0 ){
+    if ( i === 1 ){
       question2 = prompt('Введите обязательную статью расходов', 'Еда');
+      do {
+        check = +prompt('Во сколько это обойдется', 10000);
+      }
+      while ( isNaN(check) || check === '' || check === null );
     }
 
-    do {
-      check = +prompt('Во сколько это обойдется', 10000);
-    }
-    while ( isNaN(check) || check === '' || check === null );
+    
     sum += check; 
   }
   
