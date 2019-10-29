aa9095ccd8d2bffec31e28a1e65720309c41badd
Maksim <rises128@gmail.com>
2019-10-29 20:45:19 +0500

    budget_app

---
+++ lesson_7/budget_app/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let start = document.querySelector('#start'),
+    plus1 = document.querySelectorAll('button')[0],
+    plus2 = document.querySelectorAll('button')[1],
+    depositCheck = document.querySelector('#deposit-check'),
+    additionalIncomeItem = document.querySelectorAll('.additional_income-item'),
+    budgetMonthValue = document.querySelector('.budget_month-value'),
+    budgetDayValue = document.querySelector('.budget_day-value'),
+    expensesMonthValue = document.querySelector('.expenses_month-value'),
+    additionalIncomeValue = document.querySelector('.additional_income-value'),
+    additionalExpensesValue = document.querySelector('.additional_expenses-value'),
+    incomePeriodValue = document.querySelector('.income_period-value'),
+    targetMonthValue = document.querySelector('.target_month-value'),
+    salaryAmount = document.querySelector('.salary-amount'),
+    incomeItems = document.querySelector('.income-items'),
+    additionalExpensesItem = document.querySelector('.additional_expenses-item'),
+    targetAmount = document.querySelector('.target-amount'),
+    periodSelect = document.querySelector('.period-select');
+    console.log(periodSelect);
